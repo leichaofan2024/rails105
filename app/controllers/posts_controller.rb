cf3fa51f31a7ba2c8768d632b6dc8980dbda8e0d
leichaofan <leichaofan2024@gmail.com>
2017-03-10 08:33:59 +0800

    post crud

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @group = Group.find(params[:group_id])
+    @posts = Group.posts
+  end
+  def new
+    @group = Group.find(params[:group_id])
+    @post = Post.new
+  end
+  def show
+
+
+    @post = Post.find(params[:id])
+    
+  end
+  def create
+    @group = Group.find(params[:group_id])
+    @post = Post.new(post_params)
+    @post.group = @group
+    @post.user = current_user
+    if @post.save
+      redirect_to group_path(@group)
+    else
+      render :new
+    end
+  end
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
+end
