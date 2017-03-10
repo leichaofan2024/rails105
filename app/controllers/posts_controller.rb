17aea741132eb715e4d9d3cb62155aaca38e217e
leichaofan <leichaofan2024@gmail.com>
2017-03-10 18:16:58 +0800

    finished the five time

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  
+
   def index
     @group = Group.find(params[:group_id])
     @posts = Group.posts
@@ -13,6 +13,28 @@ class PostsController < ApplicationController
 
     @post = Post.find(params[:id])
 
+  end
+  def edit
+    @group = Group.find(params[:group_id ])
+    @post = Post.find(params[:id])
+  end
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.group = @group
+    @post.user = current_user
+    if @post.update
+      redirect_to group_path(@group),notie: "更新成功！"
+    else
+      render :edit
+    end
+  end
+  def destroy
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:alert] = "删除成功！"
+    redirect_to group_path(@group)
   end
   def create
     @group = Group.find(params[:group_id])
