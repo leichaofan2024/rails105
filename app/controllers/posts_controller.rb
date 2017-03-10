96b3ee8e228b237d734e01a6454aae8158a9fae6
leichaofan <leichaofan2024@gmail.com>
2017-03-10 10:19:28 +0800

    add page function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  
   def index
     @group = Group.find(params[:group_id])
     @posts = Group.posts
@@ -11,7 +12,7 @@ class PostsController < ApplicationController
 
 
     @post = Post.find(params[:id])
-    
+
   end
   def create
     @group = Group.find(params[:group_id])
